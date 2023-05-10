b51bd4fef125336952c3ea8a4fd896e4fe0fe5c5
joeehab <youssef2101042@miuegypt.edu.eg>
2023-05-10 14:16:15 +0300

    add student

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,7 +2,37 @@ const studentModel = require("../models/student");
 const instructorModel = require("../models/instructor");
 const bcrypt = require("bcrypt");
 
-const student = async (req, res) => {};
+const student = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const checkEmailFromStudent = await studentModel.findOne({ email }); //if found return the object if not return null
+    const checkUsernameFromStudent = await studentModel.findOne({ username }); //if found return the object if not return null
+
+    const checkEmailFromInstructor = await instructorModel.findOne({ email }); //if found return the object if not return null
+    const checkUsernameFromInstructor = await instructorModel.findOne({
+      username,
+    }); //if found return the object if not return null
+
+    if (checkEmailFromStudent || checkEmailFromInstructor) {
+      res.send({ msg: "Email is Taken" });
+    } else if (checkUsernameFromStudent || checkUsernameFromInstructor) {
+      res.send({ msg: "Username is Taken" });
+    } else {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const newStudent = new studentModel({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      newStudent.save();
+      res.send({ user: newStudent, msg: "Student added" });
+    }
+  } catch (err) {
+    res.json({ msg: "DATABASE ERROR" });
+  }
+};
 
 const instructor = async (req, res) => {
   const { firstName, lastName, username, email, password } = req.body;
@@ -12,7 +42,7 @@ const instructor = async (req, res) => {
 
     const checkEmailFromInstructor = await instructorModel.findOne({ email }); //if found return the object if not return null
     const checkUsernameFromInstructor = await instructorModel.findOne({
-      username
+      username,
     }); //if found return the object if not return null
 
     if (checkEmailFromStudent || checkEmailFromInstructor) {
--- frontend/src/pages/admin/AddStudent.js
+++ frontend/src/pages/admin/AddStudent.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import formValidation from "../../helper/formValidation";
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
 const AddStudent = () => {
-    return (
-        <>
-            <h1>Add Student Page</h1>
-        </>
-    );
-}
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    //check if all fields are not empty
+    const form = document.querySelector("form");
+    const error = document.getElementById("error");
+    if (formValidation(form)) {
+      error.innerHTML = formValidation(form);
+      return;
+    } else error.innerHTML = "";
+
+    try {
+      await axios
+        .post("/admin/add/student", { username, email, password })
+        .then((res) => {
+          if (res.data.msg === "Student added") {
+            document.getElementById("form").reset();
+          } else if (res.data.msg === "Username is Taken")
+            error.innerHTML = `${res.data.msg}`;
+          else if (res.data.msg === "Email is Taken")
+            error.innerHTML = `${res.data.msg}`;
+          else error.innerHTML = "Error"; //                                                      <---redirect to a check connection page
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Add Student Page</h1>
+
+      <p id="error" style={{ color: "red" }}></p>
+      <form method="POST" id="form" onSubmit={submit}>
+        <div>
+          <label htmlFor="usernameField">Username : </label>
+          <input
+            type="text"
+            id="usernameField"
+            name="username"
+            onChange={(e) => setUsername(e.target.value.trim())}
+          />
+        </div>
+        <div>
+          <label htmlFor="emailField">Email : </label>
+          <input
+            type="text"
+            id="emailField"
+            name="email"
+            onChange={(e) => setEmail(e.target.value.trim())}
+          />
+        </div>
+        <div>
+          <label htmlFor="passwordField">Password : </label>
+          <input
+            type="text"
+            id="passwordField"
+            name="password"
+            onChange={(e) => setPassword(e.target.value.trim())}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
 
 export default AddStudent;
